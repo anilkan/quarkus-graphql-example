f15d06c6a21a7c341129826b7a1fd01518c4d776
Anıl KAN <merhaba@anilkan.xyz>
2021-04-18 20:25:40 +0300

    partial update category and product objects. REST PATCH

--- src/main/java/xyz/anilkan/entity/Entity.java
+++ src/main/java/xyz/anilkan/entity/Entity.java
@@ -2,9 +2,11 @@ package xyz.anilkan.entity;
 
 import org.eclipse.microprofile.graphql.Ignore;
 
+import javax.validation.constraints.NotNull;
 import java.util.UUID;
 
 public abstract class Entity {
+    @NotNull
     private UUID id;
 
     public UUID getId() {
